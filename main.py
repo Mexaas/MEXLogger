a46cb5aeb992c94a5ceacd5e5046614748ca6f69
Mexaas <ruslancernaso@gmail.com>
2026-03-01 00:19:25 +0500

    refactor: added database initialization and more

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import os
 import disnake
 from disnake.ext import commands
+from Database import database
 
 intents = disnake.Intents().all()
 bot = commands.InteractionBot(
     intents=intents,
-    activity=disnake.CustomActivity(name="🌺 весна близко")
+    activity=disnake.CustomActivity(name="🌸 /menu")
 )
 
 @bot.event
 async def on_ready():
+    await database.init()
+
+    bot.load_extensions("Cogs/Captcha/")
+    bot.load_extensions("Cogs/Threads/")
+    bot.load_extensions("Cogs/")
+    bot.load_extensions("Events/VerifyChannel/")
+    bot.load_extensions("Events/SuggestionsChannel/")
+    bot.load_extensions("Events/GeneralDataEvent/")
+    await database.db.execute("PRAGMA journal_mode=WAL;")
+    await database.db.commit()
     print(
         f"""
         #######################
@@ -21,6 +32,4 @@ async def on_ready():
         """
     )
 
-bot.load_extensions("Cogs/Captcha/")
-bot.load_extensions("Events/VerifyChannel/")
 bot.run(os.getenv("DISCORD_TOKEN"))
