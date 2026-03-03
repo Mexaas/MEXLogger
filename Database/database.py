6891d759e7b085189ba1539d8b42c99964a72fce
Mexaas <ruslancernaso@gmail.com>
2026-03-03 08:20:26 +0500

    feat: new columns and tables to more functions

--- Database/database.py
+++ Database/database.py
@@ -8,12 +8,31 @@ async def init():
     await db.executescript(
         """
         CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER UNIQUE,
+            user_id INTEGER PRIMARY KEY,
+            user_name TEXT DEFAULT 'Нет',
+            user_age INTEGER DEFAULT 0,
+            user_description TEXT DEFAULT 'Нет',
             join_value INTEGER DEFAULT 1
         );
-
-        DROP TABLE IF EXISTS threads;
+        CREATE TABLE IF NOT EXISTS user_stats (
+            user_id INTEGER NOT NULL,
+            user_level INTEGER DEFAULT 1,
+            user_level_role INTEGER DEFAULT 1476451132560773161,
+            user_exp INTEGER DEFAULT 0,
+            user_stars INTEGER,
+            FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+        );
+        CREATE TABLE IF NOT EXISTS user_bio (
+            user_id INTEGER NOT NULL,
+            user_langs TEXT DEFAULT 'Нет',
+            user_tech TEXT DEFAULT 'Нет',
+            user_experience INTEGER DEFAULT 0,
+            user_status TEXT DEFAULT 'Нет', 
+            user_github TEXT DEFAULT 'Не указано',
+            user_gitlab TEXT DEFAULT 'Не указано',
+            user_achievements TEXT DEFAULT 'Нет',
+            FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+        );
         CREATE TABLE IF NOT EXISTS threads (
             thread_id INTEGER PRIMARY KEY,
             owner_id INTEGER NOT NULL,
@@ -23,4 +42,3 @@ async def init():
         """
     )
     await db.commit()
-
